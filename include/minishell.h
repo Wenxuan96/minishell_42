b9c272fd653547373a42226918fdd6f75ed5e2f2
Wenxuan Xi <wxi@f1r6s11.codam.nl>
2025-03-25 18:44:06 +0100

    redo everything

---
+++ include/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <ctype.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif

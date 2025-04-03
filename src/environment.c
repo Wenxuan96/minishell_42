c17c3cd6b3fd739c5996807e38a69a1f42a1a6a4
Wenxuan Xi <wxi@f1r6s8.codam.nl>
2025-04-03 16:34:56 +0200

    create command_arguments

--- src/environment.c
+++ src/environment.c
@@ -6,12 +6,28 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 11:20:32 by tanja             #+#    #+#             */
-/*   Updated: 2025/04/03 15:39:31 by wxi              ###   ########.fr       */
+/*   Updated: 2025/04/03 16:12:59 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//This test function prints all env_var
+void	prt_env_lst(t_environment **env_list)
+{
+	t_environment	**env_list_current;
+
+	env_list_current = env_list;
+	while ((*env_list_current) != NULL)
+	{
+		printf("var : %s\n", (*env_list_current)->env_var);
+		printf("val : %s\n", (*env_list_current)->value);
+		env_list_current = &(*env_list_current)->next_env_var;
+	}
+	printf("end of env list\n");
+}
+
+//This function creates a linked list of available environment variables
 int	create_env_lst(t_environment **env_list, char **envp)
 {
 	t_environment	*new_env;
@@ -22,19 +38,13 @@ int	create_env_lst(t_environment **env_list, char **envp)
 	*env_list = NULL;
 	while (envp[i] != NULL)
 	{
-		env_split = ft_split(envp[i], '=');
+		env_split = ft_split(envp[i++], '=');
 		if (!env_split)
 			return (0);
 		new_env = ft_new_var_lst(env_split[0], env_split[1]);
 		ft_var_lstadd_back(env_list, new_env);
-		i++;
 	}
-	while (env_list)
-	{
-		printf("var : %s\n", (*env_list)->env_var);
-		printf("val : %s\n", (*env_list)->value);
-		env_list = &(*env_list)->next_env_var;
-	}
+	prt_env_lst(env_list);
     return (1);
 }
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 10:41:05 by tignatov          #+#    #+#             */
-/*   Updated: 2025/04/03 15:39:04 by wxi              ###   ########.fr       */
+/*   Updated: 2025/04/03 16:14:24 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,8 @@ int	main(int argc, char **argv, char **envp)
 
 	init_shell(&shell);	
 	create_env_lst(&env_list, envp);
-	create_pipes(&shell);
-	create_processes(&shell);
-	
+	//Step 1: read from stdinput and save the input
+	// create_pipes(&shell);
+	// create_processes(&shell);
+	pause();
 }

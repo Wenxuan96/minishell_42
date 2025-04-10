d08144861e3f78cae5183354e49881d2c19b55bb
Tatiana Ignatova <tignatov@f0r6s12.codam.nl>
2025-04-10 15:24:39 +0200

    wrote pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,14 +6,20 @@
 /*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 13:07:55 by tignatov          #+#    #+#             */
-/*   Updated: 2025/04/10 13:08:32 by tignatov         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:24:11 by tignatov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
 
-// int pwd_builtin(t_process *process)
-// {
+int pwd_builtin(t_process *process)
+{
+    // char *getcwd(char *cwdbuf, size_t size);
+    char    *path_buffer;
     
-// }
+    path_buffer = getcwd(NULL, 0);
+    write(process->output_fd, path_buffer, ft_strlen(path_buffer));
+    write(process->output_fd, "\n", 1);
+    return (1);
+}

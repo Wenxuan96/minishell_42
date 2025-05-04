7bdedb4ccfd49ed58e59538468c1493d0ee40dd1
Tatiana Ignatova <tignatov@f0r1s12.codam.nl>
2025-05-04 16:06:23 +0200

    debug

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 10:41:05 by tignatov          #+#    #+#             */
-/*   Updated: 2025/05/04 15:45:07 by tignatov         ###   ########.fr       */
+/*   Updated: 2025/05/04 16:06:10 by tignatov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,6 @@
 // 	return (0);
 // }
 
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	t_minishell		shell;
-// 	// t_environment	*env_list;
-// 	// t_process		*current;
-// 	// t_process	*head;
-
-// 	(void)argc;
-// 	(void)argv;
-// 	init_shell(&shell);
-// 	create_env_lst(&shell.env_list, envp);
-// 	///redirectios here<
-// 	create_pipes(&shell);
-// 	redirections(shell.process_list);
-// 	assign_fd(&shell);
-// 	create_processes(&shell);
-// 	// head = shell.process_list;
-// 	// assign_builtin(shell.process_list, ENV, env_builtin);
-// 	// shell.process_list->builtin->function(shell.process_list);
-
-// 	// assign_builtin(shell.process_list->next_process, UNSET, unset_builtin);
-// 	// shell.process_list->next_process->builtin->function(shell.process_list->next_process);
-	
-// 	// prt_env_lst(&shell.process_list->next_process->env_vars);
-// 	// current = shell.process_list;
-// 	// printf("builtin : %i\n", current->is_builtin);
-// 	// if (current->is_builtin == 1)
-// 	// {
-// 	// 	printf("builtin type : %i\n", current->builtin->type);
-// 	// 	shell.process_list->builtin->function(current);
-// 	// }
-// 	return (0);
-// }
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -88,21 +55,5 @@ int	main(int argc, char **argv, char **envp)
 		free_everything(&shell);
 	}
 	// ft_exit(&shell, NULL); //   <--- segfaulting
-
-	// head = shell.process_list;
-	// assign_builtin(shell.process_list, ENV, env_builtin);
-	// shell.process_list->builtin->function(shell.process_list);
-
-	// assign_builtin(shell.process_list->next_process, UNSET, unset_builtin);
-	// shell.process_list->next_process->builtin->function(shell.process_list->next_process);
-	
-	// prt_env_lst(&shell.process_list->next_process->env_vars);
-	// current = shell.process_list;
-	// printf("builtin : %i\n", current->is_builtin);
-	// if (current->is_builtin == 1)
-	// {
-	// 	printf("builtin type : %i\n", current->builtin->type);
-	// 	shell.process_list->builtin->function(current);
-	// }
 	return (0);
 }

9bcfe9ef1109c0994b9c8d4d86ecc905bc028c7c
Wenxuan Xi <wxi@f0r3s6.codam.nl>
2025-05-20 16:37:03 +0200

    add

--- include/minishell.h
+++ include/minishell.h
@@ -1,11 +1,11 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
+#include <signal.h>
 #include <ctype.h>
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>

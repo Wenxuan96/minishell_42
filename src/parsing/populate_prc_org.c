91f799f53fc4724b532f85091ceeb573be888872
Wenxuan Xi <wxi@f0r3s6.codam.nl>
2025-05-20 15:39:27 +0200

    add

---
+++ src/parsing/populate_prc_org.c
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   populate_process.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 21:36:07 by wxi               #+#    #+#             */
+/*   Updated: 2025/05/02 13:48:04 by tignatov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "minishell.h"
+
+// int	count_pipes(t_minishell *shell)
+// {
+// 	int		i;
+// 	t_token	*current;
+	
+// 	i = 1;
+// 	current = shell->token_list;
+// 	while (current)
+// 	{
+// 		if ((ft_strcmp(current->token_val, "|") == 0)
+// 			&& (ft_strlen(current->token_val) == 1) && 
+// 			current->type == PIPELINE)
+// 			i++;	
+// 		current = current->next_token;
+// 	}
+// 	return (i);
+// }
+
+// char	**get_commands(t_token	**current)
+// {
+// 	int		i;
+// 	char	**commands;
+// 	t_token	*process_current;
+	
+// 	i = 0;
+// 	if ((*current)->type == PIPELINE && (*current)->next_token != NULL)
+// 		*current = (*current)->next_token;
+// 	process_current = *current;
+// 	while (process_current && process_current->type != PIPELINE)
+// 	{
+// 		i++;
+// 		process_current = process_current->next_token;
+// 	}
+// 	commands = (char **)malloc((i + 1) * sizeof(char *));
+// 	if (!commands)
+// 		return (NULL);
+// 	i = 0;
+// 	while ((*current) && (*current)->type != PIPELINE)
+// 	{
+// 		commands[i] = strdup((*current)->token_val);
+//         printf("commands: %s\n", commands[i]);
+// 		(*current) = (*current)->next_token;
+// 		i++;
+// 	}
+// 	commands[i] = NULL;
+// 	return (commands);
+// }
+
+// void prt_cmds(t_process *process_lst)
+// {
+// 	t_process *current;
+// 	int			i;
+	
+// 	i = 0;
+// 	current = process_lst;
+// 	while(current)
+// 	{
+// 		ft_printf("process commands are: ");
+// 		while (current->command_arguments[i])
+// 			ft_printf("[%s] ", current->command_arguments[i++]);
+// 		current = current->next_process;
+// 		i = 0;
+// 	}	
+// }
+
+// // int create_pipes(t_minishell *shell)
+// // {
+// // 	t_process   *process_lst;
+// // 	t_process   *current;
+// // 	// char *p1_cmd[] = {"ls", NULL};
+// // 	// char *p2_cmd[] = {"grep", "test", NULL};
+// // 	// char *p3_cmd[] = {"wc", "-l", NULL};
+
+// // 	char *p1_cmd[] = {"echo", "hello", NULL};
+// // 	char *p2_cmd[] = {"cat", NULL};
+// // 	char *p3_cmd[] = {"wc", "-c", NULL};
+
+// // 	process_lst = NULL;
+// // 	process_lst_add_back(new_process_lst(shell, p1_cmd), &process_lst);
+// // 	process_lst_add_back(new_process_lst(shell, p2_cmd), &process_lst);
+// // 	process_lst_add_back(new_process_lst(shell, p3_cmd), &process_lst);
+// // 	shell->process_list = process_lst;
+// // 	current = process_lst;
+// // 	while (current != NULL)
+// // 	{
+// // 		// printf("%s\n", current->command_arguments[0]);
+// // 		current = current->next_process;
+// // 		shell->num_processes++;
+// // 	}
+// // 	current = process_lst;
+// // 	if (shell->num_processes > 1)
+// // 	{
+// // 		while (current != NULL)
+// // 		{
+// // 			current->is_pipeline = 1;
+// // 			current = current->next_process;
+// // 		}
+// // 	}
+// // 	shell->pipes = allocate_pipes(shell->num_processes - 1);
+// // 	return (1);
+// // }
+
+// int init_processlst(t_minishell *shell)
+// {
+// 	int			n_processes;
+// 	char		**arr_commands;
+// 	t_process	*process_lst;
+// 	t_token		*current;
+// 	int			i;
+
+// 	i = 0;
+// 	current = shell->token_list;
+// 	process_lst = NULL;
+// 	n_processes = count_pipes(shell);
+// 	while (i < n_processes)
+// 	{
+// 		arr_commands = get_commands(&current);
+// 		if (!arr_commands)
+//             return (0);
+// 		if (!process_lst)
+// 			process_lst = new_process_lst(shell, arr_commands);
+// 		else
+// 			process_lst_add_back(new_process_lst(shell, arr_commands), &process_lst);
+// 		i++;
+// 	}
+// 	prt_cmds(process_lst);
+// 	return (1);
+// }
--- src/parsing/populate_process.c
+++ src/parsing/populate_process.c
@@ -1,142 +1,240 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   populate_process.c                                 :+:      :+:    :+:   */
+/*   populate_process__2.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 21:36:07 by wxi               #+#    #+#             */
-/*   Updated: 2025/05/02 13:48:04 by tignatov         ###   ########.fr       */
+/*   Updated: 2025/05/13 12:51:24 by tignatov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
+#include "minishell.h"
+#include "builtins.h"
 
-// int	count_pipes(t_minishell *shell)
-// {
-// 	int		i;
-// 	t_token	*current;
+int	count_pipes(t_minishell *shell)
+{
+	int		i;
+	t_token	*current;
 	
-// 	i = 1;
-// 	current = shell->token_list;
-// 	while (current)
-// 	{
-// 		if ((ft_strcmp(current->token_val, "|") == 0)
-// 			&& (ft_strlen(current->token_val) == 1) && 
-// 			current->type == PIPELINE)
-// 			i++;	
-// 		current = current->next_token;
-// 	}
-// 	return (i);
-// }
+	i = 1;
+	current = shell->token_list;
+	while (current)
+	{
+		if ((ft_strcmp(current->token_val, "|") == 0)
+			&& (ft_strlen(current->token_val) == 1) && 
+			current->type == PIPELINE)
+			i++;	
+		current = current->next_token;
+	}
+	return (i);
+}
 
-// char	**get_commands(t_token	**current)
-// {
-// 	int		i;
-// 	char	**commands;
-// 	t_token	*process_current;
+char	**get_commands(t_token	**token)
+{
+	int		i;
+	char	**commands;
+	t_token	*current;
 	
-// 	i = 0;
-// 	if ((*current)->type == PIPELINE && (*current)->next_token != NULL)
-// 		*current = (*current)->next_token;
-// 	process_current = *current;
-// 	while (process_current && process_current->type != PIPELINE)
-// 	{
-// 		i++;
-// 		process_current = process_current->next_token;
-// 	}
-// 	commands = (char **)malloc((i + 1) * sizeof(char *));
-// 	if (!commands)
-// 		return (NULL);
-// 	i = 0;
-// 	while ((*current) && (*current)->type != PIPELINE)
-// 	{
-// 		commands[i] = strdup((*current)->token_val);
-//         printf("commands: %s\n", commands[i]);
-// 		(*current) = (*current)->next_token;
-// 		i++;
-// 	}
-// 	commands[i] = NULL;
-// 	return (commands);
-// }
+	i = 0;
+	if ((*token)->type == PIPELINE && (*token)->next_token != NULL)
+		*token = (*token)->next_token;
+	current = *token;
+	while (current && current->type != PIPELINE)
+	{
+		if (current->type == REDIRECTION)
+		{
+			current = current->next_token;
+			if (current)
+				current = current->next_token;
+		}
+		else
+		{
+			i++;
+			current = current->next_token;
+		}
+	}
+	commands = (char **)malloc((i + 1) * sizeof(char *));
+	if (!commands)
+		return (NULL);
+	i = 0;
+	while ((*token) && (*token)->type != PIPELINE)
+	{
+		if ((*token)->type == REDIRECTION)
+		{
+			(*token) = (*token)->next_token;
+			if (*token != NULL)
+				(*token) = (*token)->next_token;
+		}
+		else
+		{
+			commands[i] = strdup((*token)->token_val);
+			if (!commands[i])
+			{
+				free_2darray(commands);
+                return (NULL);
+			}
+			(*token) = (*token)->next_token;
+			i++;
+		}
+	}
+	commands[i] = NULL;
+	return (commands);
+}
 
-// void prt_cmds(t_process *process_lst)
-// {
-// 	t_process *current;
-// 	int			i;
+void prt_cmds(t_process *process_lst)
+{
+	t_process *current;
+	int			i;
 	
-// 	i = 0;
-// 	current = process_lst;
-// 	while(current)
-// 	{
-// 		ft_printf("process commands are: ");
-// 		while (current->command_arguments[i])
-// 			ft_printf("[%s] ", current->command_arguments[i++]);
-// 		current = current->next_process;
-// 		i = 0;
-// 	}	
-// }
+	i = 0;
+	current = process_lst;
+	while(current)
+	{
+		ft_printf("process commands are: ");
+		while (current->command_arguments[i])
+			ft_printf("[%s] ", current->command_arguments[i++]);
+		current = current->next_process;
+		i = 0;
+	}	
+}
 
-// // int create_pipes(t_minishell *shell)
-// // {
-// // 	t_process   *process_lst;
-// // 	t_process   *current;
-// // 	// char *p1_cmd[] = {"ls", NULL};
-// // 	// char *p2_cmd[] = {"grep", "test", NULL};
-// // 	// char *p3_cmd[] = {"wc", "-l", NULL};
+t_redir_type	get_redir_type(t_token *token)
+{
+	t_redir_type	type;
 
-// // 	char *p1_cmd[] = {"echo", "hello", NULL};
-// // 	char *p2_cmd[] = {"cat", NULL};
-// // 	char *p3_cmd[] = {"wc", "-c", NULL};
+	type = NONE;
+	if (ft_strcmp(token->token_val, ">") == 0 && token->token_val[1] == '\0')
+		type = OUTPUT;
+	else if (ft_strcmp(token->token_val, "<") == 0 && token->token_val[1] == '\0')
+		type = INPUT;
+	else if (ft_strcmp(token->token_val, ">>") == 0 && token->token_val[2] == '\0')
+		type = OUTPUT_APPEND;
+	else if (ft_strcmp(token->token_val, "<<") == 0 && token->token_val[2] == '\0')
+		type = HEREDOC;
+	return (type);
+}
+char	*get_file(t_token *token)
+{
+	char	*redir_file;
 
-// // 	process_lst = NULL;
-// // 	process_lst_add_back(new_process_lst(shell, p1_cmd), &process_lst);
-// // 	process_lst_add_back(new_process_lst(shell, p2_cmd), &process_lst);
-// // 	process_lst_add_back(new_process_lst(shell, p3_cmd), &process_lst);
-// // 	shell->process_list = process_lst;
-// // 	current = process_lst;
-// // 	while (current != NULL)
-// // 	{
-// // 		// printf("%s\n", current->command_arguments[0]);
-// // 		current = current->next_process;
-// // 		shell->num_processes++;
-// // 	}
-// // 	current = process_lst;
-// // 	if (shell->num_processes > 1)
-// // 	{
-// // 		while (current != NULL)
-// // 		{
-// // 			current->is_pipeline = 1;
-// // 			current = current->next_process;
-// // 		}
-// // 	}
-// // 	shell->pipes = allocate_pipes(shell->num_processes - 1);
-// // 	return (1);
-// // }
+	redir_file = NULL;
+	if (token->next_token->type == WORD)
+		redir_file = token->next_token->token_val;
+	return (redir_file);
+}
 
-// int init_processlst(t_minishell *shell)
-// {
-// 	int			n_processes;
-// 	char		**arr_commands;
-// 	t_process	*process_lst;
-// 	t_token		*current;
-// 	int			i;
+int	parse_redirection(t_minishell *shell)
+{
+	t_process *current_process;
+	t_token		*current_token;
+	t_redir_type type;
+	char 		*file;
 
-// 	i = 0;
-// 	current = shell->token_list;
-// 	process_lst = NULL;
-// 	n_processes = count_pipes(shell);
-// 	while (i < n_processes)
-// 	{
-// 		arr_commands = get_commands(&current);
-// 		if (!arr_commands)
-//             return (0);
-// 		if (!process_lst)
-// 			process_lst = new_process_lst(shell, arr_commands);
-// 		else
-// 			process_lst_add_back(new_process_lst(shell, arr_commands), &process_lst);
-// 		i++;
-// 	}
-// 	prt_cmds(process_lst);
-// 	return (1);
-// }
+	current_process = shell->process_list;
+	current_token = shell->token_list;
+	file = NULL;
+	if (current_token->type == PIPELINE && current_token->next_token != NULL)
+		current_token = current_token->next_token;
+	while (current_process != NULL)
+	{
+		while (current_token != NULL)
+		{
+			if (current_token->type == PIPELINE)
+			{
+				current_token = current_token->next_token;
+				break ;
+			}
+			if (current_token->type == REDIRECTION)
+			{
+				type = get_redir_type(current_token);
+				file = ft_strdup(get_file(current_token));
+				if (!file)
+					return (0);
+				if (!current_process->redirections)
+				{
+					current_process->redirections = new_redir_lst(type, file);
+					if (!current_process->redirections)
+						return (0);
+				}
+				else
+					redir_lst_add_back(new_redir_lst(type, file), &current_process->redirections);
+				current_token = current_token->next_token;
+				if (current_token->next_token)
+					current_token = current_token->next_token;
+				}
+			else
+				current_token = current_token->next_token;
+		}
+		// if (current_process->redirections)
+		// {
+		// 	printf("redirection type: %u\n", current_process->redirections->type);
+		// 	printf("redirection file: %s\n", current_process->redirections->file);
+		// }
+		current_process = current_process->next_process;
+	}
+	return (1);
+}
+
+void	print_process(t_minishell *shell)
+{
+	t_process   *current;
+	t_redirection *curr_redir;
+
+    current = shell->process_list;
+	printf("num_proc: %i\n", shell->num_processes);
+    while (current != NULL)
+	{
+		curr_redir = current->redirections;
+		printf("command :%s, %s\n", current->command_arguments[0], current->command_arguments[1]);
+		if (curr_redir)
+		{
+			while (curr_redir)
+			{
+				printf("redirection type: %u\n", curr_redir->type);
+				printf("redirection file: %s\n", curr_redir->file);
+				curr_redir = curr_redir->next_redir;
+			}
+		}
+		if (current->is_builtin == 1)
+			printf("type: %u\n", current->builtin->type);
+		current = current->next_process;
+	}
+}
+
+int init_processlst(t_minishell *shell)
+{
+	char		**arr_commands;
+	// t_process	*process_lst;
+	t_token		*current;
+	int			i;
+
+	i = 0;
+	current = shell->token_list;
+	shell->num_processes = count_pipes(shell);
+	// printf("num proc: %i\n", shell->num_processes);
+	while (i < shell->num_processes)
+	{
+		arr_commands = get_commands(&current);
+		if (!arr_commands)
+			exit_with_error(shell, "memory allocation failed", EXEC_FAILURE);
+		if (!shell->process_list)
+		{
+			shell->process_list = new_process_lst(shell, arr_commands);
+			if (!shell->process_list)
+				exit_with_error(shell, "memory allocation failed", EXEC_FAILURE);
+		}
+		else
+			process_lst_add_back(new_process_lst(shell, arr_commands), &shell->process_list);
+		free_2darray(arr_commands);
+		i++;
+	}
+	// free_2darray(arr_commands);
+	if (!parse_redirection(shell))
+		exit_with_error(shell, "memory allocation failed", EXEC_FAILURE);
+	parse_builtin(shell);
+	// print_process(shell);
+	// prt_cmds(shell->process_list);
+	return (1);
+}

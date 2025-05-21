f6ba12f1c3646ab88561985ef7d3689628e2b352
Wenxuan Xi <wxi@f0r1s13.codam.nl>
2025-05-21 16:21:42 +0200

    add

--- src/parsing/populate_process.c
+++ src/parsing/populate_process.c
@@ -6,56 +6,12 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 21:36:07 by wxi               #+#    #+#             */
-/*   Updated: 2025/05/21 16:08:40 by wxi              ###   ########.fr       */
+/*   Updated: 2025/05/21 16:16:07 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "builtins.h"
-
-int	count_pipes(t_minishell *shell)
-{
-	int		i;
-	t_token	*current;
-	
-	i = 1;
-	current = shell->token_list;
-	while (current)
-	{
-		if ((ft_strcmp(current->token_val, "|") == 0)
-			&& (ft_strlen(current->token_val) == 1) && 
-			current->type == PIPELINE)
-			i++;	
-		current = current->next_token;
-	}
-	return (i);
-}
-
-static int	count_commands(t_token	**token)
-{
-	int		i;
-	t_token	*current;
-
-	i = 0;
-	if ((*token)->type == PIPELINE && (*token)->next_token != NULL)
-		*token = (*token)->next_token;
-	current = *token;
-	while (current && current->type != PIPELINE)
-	{
-		if (current->type == REDIRECTION)
-		{
-			current = current->next_token;
-			if (current)
-				current = current->next_token;
-		}
-		else
-		{
-			i++;
-			current = current->next_token;
-		}
-	}
-	return (i);
-}
 
 char	**get_commands(t_token	**token)
 {
@@ -90,23 +46,6 @@ char	**get_commands(t_token	**token)
 	return (commands);
 }
 
-void prt_cmds(t_process *process_lst)
-{
-	t_process *current;
-	int			i;
-	
-	i = 0;
-	current = process_lst;
-	while(current)
-	{
-		ft_printf("process commands are: ");
-		while (current->command_arguments[i])
-			ft_printf("[%s] ", current->command_arguments[i++]);
-		current = current->next_process;
-		i = 0;
-	}	
-}
-
 t_redir_type	get_redir_type(t_token *token)
 {
 	t_redir_type	type;
@@ -122,15 +61,8 @@ t_redir_type	get_redir_type(t_token *token)
 		type = HEREDOC;
 	return (type);
 }
-char	*get_file(t_token *token)
-{
-	char	*redir_file;
 
-	redir_file = NULL;
-	if (token->next_token->type == WORD)
-		redir_file = token->next_token->token_val;
-	return (redir_file);
-}
+
 
 int	parse_redirection(t_minishell *shell)
 {
@@ -177,32 +109,6 @@ int	parse_redirection(t_minishell *shell)
 		current_process = current_process->next_process;
 	}
 	return (1);
-}
-
-void	print_process(t_minishell *shell)
-{
-	t_process   *current;
-	t_redirection *curr_redir;
-
-    current = shell->process_list;
-	printf("num_proc: %i\n", shell->num_processes);
-    while (current != NULL)
-	{
-		curr_redir = current->redirections;
-		printf("command :%s, %s\n", current->command_arguments[0], current->command_arguments[1]);
-		if (curr_redir)
-		{
-			while (curr_redir)
-			{
-				printf("redirection type: %u\n", curr_redir->type);
-				printf("redirection file: %s\n", curr_redir->file);
-				curr_redir = curr_redir->next_redir;
-			}
-		}
-		if (current->is_builtin == 1)
-			printf("type: %u\n", current->builtin->type);
-		current = current->next_process;
-	}
 }
 
 int init_processlst(t_minishell *shell)

07a03516a1ebac44a501dfb0df7b05b41a610ad3
Wenxuan Xi <wxi@f0r4s25.codam.nl>
2025-05-26 12:13:38 +0200

    add

--- src/parsing/variable_expansion.c
+++ src/parsing/variable_expansion.c
@@ -6,7 +6,7 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 16:28:10 by wxi               #+#    #+#             */
-/*   Updated: 2025/05/24 20:14:26 by wxi              ###   ########.fr       */
+/*   Updated: 2025/05/26 12:12:26 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ bool	find_dollar(char *token_val)
 	return(dollar_found);
 }
 
+char	*ft_getenv(char *var_name, t_minishell *shell)
+{
+	char	*var_val;
+	t_environment	*current;
+
+	var_val = NULL;
+	current = shell->env_list;
+	while(current != NULL)
+	{
+		if (ft_strcmp(current->env_var, var_name) == 0)
+			return	(current->value);
+		current = current->next_env_var;
+	}
+	return (var_val);
+}
 
 char	*expand_token(t_token *token, t_minishell *shell)
 {
@@ -61,7 +76,7 @@ char	*expand_token(t_token *token, t_minishell *shell)
 			var_name = ft_substr(result, var_start, i - var_start);
 			if (!var_name)
 				display_shell_error2(shell, "memory allocation failed", EXEC_FAILURE);
-			var_val = getenv(var_name);
+			var_val = ft_getenv(var_name, shell);
 			if (!var_val)
 				var_val = "";
 			before = ft_substr(result, 0, var_start - 1);
@@ -87,8 +102,12 @@ char *def_expansion(t_token *token, t_minishell *shell)
 	char	*commands;
 
 	commands = NULL;
-	if (find_dollar(token->token_val) == true && token->double_quoted == true)
+	if (find_dollar(token->token_val) == true && 
+			(token->in_quotes == false || token->double_quoted == true))
+	{
+		printf("expanded\n");
 		commands = expand_token(token, shell);
+	}
 	else
 		commands = strdup(token->token_val);
 	return (commands);

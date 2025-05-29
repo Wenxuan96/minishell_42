3cba4e6c95ad3bec43185164088213f6a3fb70ab
Tatiana Ignatova <tignatov@f0r6s10.codam.nl>
2025-05-29 17:01:21 +0200

    debugged cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:31:54 by tignatov          #+#    #+#             */
-/*   Updated: 2025/05/29 12:16:34 by tignatov         ###   ########.fr       */
+/*   Updated: 2025/05/29 16:57:48 by tignatov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,23 @@ int	cd_builtin(t_process *process, t_minishell *shell)
     int     len;
 
     (void)shell;
+    if (process->command_arguments[2] != NULL)
+        return(display_shell_error(process, "cd: too many arguments", EXEC_FAILURE), 0);
     path = process->command_arguments[1];
     if (chdir(path) == -1)
-        return(display_shell_error(process, "No such file or directory", EXEC_FAILURE), 0);
+    {
+        if (errno == ENOENT)
+            return (display_shell_error(process, "cd: no such file or directory", EXEC_FAILURE), 0);
+        else if (errno == EACCES)
+            return (display_shell_error(process, "cd: permission denied", EXEC_FAILURE), 0);
+        else if (errno == ENOTDIR)
+            return (display_shell_error(process, "cd: not a directory", EXEC_FAILURE), 0);
+        else
+            return (display_shell_error(process, "cd: error", EXEC_FAILURE), 0);
+    }
     new_path_buffer = getcwd(NULL, 0);
     if (!new_path_buffer)
-        return(display_shell_error(process, "getcwd failed", EXEC_FAILURE), 0);
+        return(display_shell_error(process, "cd: getcwd failed", EXEC_FAILURE), 0);
     while (shell->env_list != NULL)
     {
         len = ft_strlen(shell->env_list->env_var);
@@ -34,7 +45,7 @@ int	cd_builtin(t_process *process, t_minishell *shell)
             free(shell->env_list->value);
             shell->env_list->value = ft_strdup(new_path_buffer);
             if (!shell->env_list->value)
-                return (free(new_path_buffer), display_shell_error(process, "memory allocation failed", EXEC_FAILURE), 0);
+                return (free(new_path_buffer), display_shell_error(process, "cd: memory allocation failed", EXEC_FAILURE), 0);
             // printf("new pwd: %s\n", shell->env_list->value);
             break;
         }

d5b607050a5b240d5c587e1509fdefa277770be8
Wenxuan Xi <wxi@f1r1s12.codam.nl>
2025-06-30 21:14:54 +0200

    Aa

--- src/parsing/input_checker.c
+++ src/parsing/input_checker.c
@@ -6,7 +6,7 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 16:31:52 by wxi               #+#    #+#             */
-/*   Updated: 2025/06/30 20:21:16 by wxi              ###   ########.fr       */
+/*   Updated: 2025/06/30 21:14:10 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,42 @@ static int wit_space_str(t_minishell *shell)
 	return(1);
 }
 
+static int	skip_white_space(t_minishell *shell)
+{
+	int		start;
+	int		end;
+	char	*tmp;
+
+	start = 0;
+	while (shell->input_str[start] == ' ' || shell->input_str[start] == '\t')
+		start++;
+	end = ft_strlen(shell->input_str) - 1;
+	while (end >= start && (shell->input_str[end] == ' ' || shell->input_str[end] == '\t'))
+		end--;
+	tmp = ft_substr(shell->input_str, start, end - start + 1);
+	if (!tmp)
+		return (EXEC_FAILURE);
+	free(shell->input_str);
+	shell->input_str = tmp;
+	return (EXEC_SUCCESS);
+}
+
+static int	special_chr_ctrl(t_minishell *shell)
+{
+	if (skip_white_space(shell) == EXEC_FAILURE)
+	{
+		display_shell_error2(shell, "memory allocation failed", EXEC_FAILURE);
+		return (EXEC_FAILURE);
+	}
+	return (EXEC_SUCCESS);
+}
+
 int	read_input(int argc, t_minishell *shell)
 {
 	int	init;
-
+	int	error;
+	
+	error = EXEC_SUCCESS;
 	init = 0;
 	if (init == 0 && argc != 1)
 		ft_exit(NULL, "Expecting <./minishell> to start the program.");
@@ -63,18 +95,17 @@ int	read_input(int argc, t_minishell *shell)
 	shell->input_str = readline("minishell$ ");
 	if (!shell->input_str)
 		ft_exit(shell, "exit");
-	if (shell->input_str[0] == '\0' || wit_space_str(shell) == 1)
-		return (2);
-	if (ft_strcmp(shell->input_str, "\'\'") == 0 ||
+	else if (shell->input_str[0] == '\0' || wit_space_str(shell) == 1)
+		error = EX_BADUSAGE;
+	else if (ft_strcmp(shell->input_str, "\'\'") == 0 ||
 		ft_strcmp(shell->input_str, "\"\"") == 0)
-		return (CMD_NOTFOUND);
+		error = CMD_NOTFOUND;
+	error = special_chr_ctrl(shell);
 	add_history(shell->input_str);
-	if (tokenize_input(shell) != EXEC_SUCCESS)
-		return (EXEC_FAILURE);
+	if (tokenize_input(shell) != EXEC_SUCCESS || init_processlst(shell) != EXEC_SUCCESS)
+		error = EXEC_FAILURE;
 	// prt_tokenlst(shell);
-	if (init_processlst(shell) != EXEC_SUCCESS)
-		return (EXEC_FAILURE);
 	free_tokenlst(shell);
 	free(shell->input_str);
-	return(EXEC_SUCCESS);
+	return(error);
 }

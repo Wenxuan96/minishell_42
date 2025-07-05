a3f6ff150d223878182997d0f33f1cb4d810bd39
Wenxuan Xi <wxi@f0r6s10.codam.nl>
2025-07-05 17:26:54 +0200

    add

--- src/utils_clear.c
+++ src/utils_clear.c
@@ -6,7 +6,7 @@
 /*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:31:22 by wxi               #+#    #+#             */
-/*   Updated: 2025/07/05 17:13:30 by wxi              ###   ########.fr       */
+/*   Updated: 2025/07/05 17:16:24 by wxi              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,6 @@ void	ft_lstclear_token(t_token **token_list)
 		current = next;
 	}
 	*token_list = NULL;
-}
-
-void	free_2darray(char **arr)
-{
-	int	i;
-
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
 }
 
 void	ft_lstclear_env(t_environment **env_list)
@@ -111,31 +98,6 @@ void ft_lstclear_process(t_process **process_list)
     *process_list = NULL;
 }
 
-void	free_pipes2(int **pipes, int p_num)
-{
-	int	i;
-	
-	i = 0;
-	while (i < p_num)
-	{
-		free(pipes[i]);
-		i++;
-	}
-	free(pipes);
-	pipes = NULL;
-}
-
-void    free_process(t_minishell *shell, t_process *current)
-{
-    if (shell->pipes)
-        close_pipe_ends(shell, current);
-    if (shell->env_list)
-        ft_lstclear_env(&shell->env_list);
-    ft_lstclear_process(&shell->process_list); 
-    if (shell->pipes)
-        free_pipes(shell);
-}
-
 void	ft_exit(t_minishell *shell, char *error_msg)
 {
 	if (error_msg)
@@ -157,22 +119,4 @@ void	ft_exit(t_minishell *shell, char *error_msg)
 		shell = NULL;
 	}
 	exit(EXEC_SUCCESS);
-}
-
-void	ft_clear_shell(t_minishell *shell)
-{
-	if (shell)
-	{
-		ft_lstclear_token(&shell->token_list);
-		ft_lstclear_env(&shell->env_list);
-		ft_lstclear_process(&shell->process_list);
-		if (shell->input_str)
-			free (shell->input_str);
-		if (shell->pipes)
-		{
-			free_pipes(shell);
-			shell->pipes = NULL;
-		}
-		shell = NULL;
-	}
 }
---
+++ src/utils_free.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_free.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 17:14:38 by wxi               #+#    #+#             */
+/*   Updated: 2025/07/05 17:16:29 by wxi              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "./builtins/builtins.h"
+
+void	free_2darray(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+void	free_pipes2(int **pipes, int p_num)
+{
+	int	i;
+	
+	i = 0;
+	while (i < p_num)
+	{
+		free(pipes[i]);
+		i++;
+	}
+	free(pipes);
+	pipes = NULL;
+}
+
+void    free_process(t_minishell *shell, t_process *current)
+{
+    if (shell->pipes)
+        close_pipe_ends(shell, current);
+    if (shell->env_list)
+        ft_lstclear_env(&shell->env_list);
+    ft_lstclear_process(&shell->process_list); 
+    if (shell->pipes)
+        free_pipes(shell);
+}
+
+void	ft_clear_shell(t_minishell *shell)
+{
+	if (shell)
+	{
+		ft_lstclear_token(&shell->token_list);
+		ft_lstclear_env(&shell->env_list);
+		ft_lstclear_process(&shell->process_list);
+		if (shell->input_str)
+			free (shell->input_str);
+		if (shell->pipes)
+		{
+			free_pipes(shell);
+			shell->pipes = NULL;
+		}
+		shell = NULL;
+	}
+}

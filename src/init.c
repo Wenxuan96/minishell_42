5751c6ac0b05f7fc892304481aee5e760c3f350f
Tatiana Ignatova <tignatov@f0r3s26.codam.nl>
2025-07-07 18:01:46 +0200

    fff

--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wxi <wxi@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: tignatov <tignatov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:00:12 by tignatov          #+#    #+#             */
-/*   Updated: 2025/07/07 14:52:44 by wxi              ###   ########.fr       */
+/*   Updated: 2025/07/07 18:01:37 by tignatov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 void	init_shell(t_minishell *shell)
 {
-	static const char *const system_commands[] = {"ls", "cat", "grep",
-		"wc", "head", "tail", "chmod", "mv", "cp", "rm", "mkdir", "ps", "kill", "./minishell", NULL};
-	static const char *const buildin_commands[] = {"cd", "echo", "exit",
-		"pwd", "env", "unset", "alias", "export", "type", "set", "history", NULL};
+	static const char *const system_commands[] = {"ls", "cat", "grep", "wc",
+		"head", "tail", "chmod", "mv", "cp", "rm", "mkdir", "ps", "kill",
+		"./minishell", NULL};
+	static const char *const buildin_commands[] = {"cd", "echo", "exit", "pwd",
+		"env", "unset", "alias", "export", "type", "set", "history", NULL};
 	shell->system_commands = (const char **)system_commands;
 	shell->buildin_commands = (const char **)buildin_commands;
 	shell->input_str = NULL;
@@ -26,31 +27,14 @@ void	init_shell(t_minishell *shell)
 	shell->num_processes = 0;
 	shell->input_status = -1;
 	shell->env_list = NULL;
-	shell->pipes= NULL;
+	shell->pipes = NULL;
 	shell->exit_status = 0;
 	shell->heredoc_inquote = false;
 }
 
-// t_process	*new_process_lst(char **commands)
-// {
-// 	t_process	*new_process;
-	
-// 	new_process = malloc(sizeof(t_process));
-// 	new_process->command_arguments = allocate_array(commands);
-// 	new_process->redirections= NULL;
-// 	new_process->input_fd = -1;
-// 	new_process->is_pipeline = false;
-// 	new_process->output_fd = -1;
-// 	new_process->next_process= NULL;
-// 	new_process->is_builtin = false;
-// 	new_process->builtin = NULL;
-// 	new_process->stopped = 0;
-// 	new_process->completed = 0;
-// }
-
 t_token	*new_token_lst(char *token)
 {
-	t_token *new_token;
+	t_token	*new_token;
 
 	new_token = NULL;
 	new_token = malloc(sizeof(t_token));
